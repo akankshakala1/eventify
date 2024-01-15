65477995dd97e2996586cf8e567b3e62042367c3
akankshakala1 <akankshakala332@gmail.com>
2024-01-15 17:33:41 +0300

    create events and invitaion models

--- event/models.py
+++ event/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="events")
+    organizer = models.ForeignKey(User, related_name='organized_events', on_delete=models.CASCADE)
+    attendees = models.ManyToManyField(User, related_name='attended_events', blank=True)
+
+class Invitation(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_invitations')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_invitations')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    accepted = models.BooleanField(default=False)
+
