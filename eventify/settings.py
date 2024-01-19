b3813a6820ec8c844f8f44cc3398a183a3572d0f
akankshakala1 <akankshakala332@gmail.com>
2024-01-19 20:40:43 +0300

    collect static files

--- eventify/settings.py
+++ eventify/settings.py
@@ -97,6 +97,16 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '$eventify',
+#         'HOST': 'akankshakala1.mysql.pythonanywhere-services.com',
+#         'USER': 'akankshakala1',
+#         'PASSWORD': 'Qwerty586311'
+#     }
+# }
+
 
 load_dotenv()
 
@@ -154,7 +164,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
